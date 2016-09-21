d64b12c36f6b0a57964f879dac90a5b64c8c2bb5
boyboi86 <boyboi86@gmail.com>
2016-09-21 23:21:41 +0800

    included pluggins

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 const path = require('path');
+const webpack = require('webpack');
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   entry: [
     './src/index.js'
   ],
   output: {
-    path: path.resolve(__dirname,'/src'),
+    path: path.resolve(__dirname, 'src'),
     filename: 'bundle.js'
   },
+  devtool: debug ? "inline-sourcemap" : null,
   module: {
     loader: [{
       exclude: /node_modules/,
@@ -20,5 +23,14 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: 'src'
-  }
-};
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+    mangle: {except: ['$', 'exports', 'require', 'app']},
+    compress: {warnings: false},
+    sourceMap: false
+  })
+  ]
+}
